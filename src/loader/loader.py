3046e3d820ff835f18d077d657e1e959dd5f6560
Dominik Frey <frey@brickwork.ch>
2023-08-24 16:44:44 +0200

    asd-2 add Trace

--- src/loader/loader.py
+++ src/loader/loader.py
@@ -1,12 +1,38 @@
+import time
+
 import h5py
 import numpy as np
+from h5py import AttributeManager
+
+from src.domain.Trace import Trace
 
 FILEPATH = '../../../../Data/'
 FILENAME = "Sz2.h5"
+TRACES = 'traces'
+RAW_DATA = 'raw'
+
+
+def create_trace(label: str, dataset: h5py.Dataset, attributes: AttributeManager):
+    return Trace(label,
+                 attributes.get('duration'),
+                 attributes.get('n_samples'),
+                 attributes.get('processing'),
+                 dataset.attrs.get('sfreq'),
+                 dataset.attrs.get('unit'),
+                 attributes.get('start_date'),
+                 attributes.get('start_time'),
+                 attributes.get('start_timestamp'),
+                 dataset[:])
 
 
 def read_h5_file(path):
-    return h5py.File(path, "r")
+    h5_file = h5py.File(path, "r")
+    raw_traces = h5_file.get(TRACES).get(RAW_DATA)
+    attributes = raw_traces.attrs
+    return [create_trace(label, raw_traces.get(label), attributes) for label in raw_traces.keys()]
 
 
-h5_file = read_h5_file(FILEPATH + FILENAME)
+start = time.time()
+datasets = read_h5_file(FILEPATH + FILENAME)
+size = len(datasets)
+print(f'elapsed time: {(time.time() - start)/1000} seconds')
---
+++ src/preprocessing/filter.py
@@ -0,0 +1,6 @@
+import mne
+
+
+# create bandpass filter for 1 - 200 Hz
+def filter_signal(data):
+    pass
