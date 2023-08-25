84db7533c0d755a470707b749c260ba8c6d670f2
Dominik Frey <frey@brickwork.ch>
2023-08-25 13:30:43 +0200

    asd-5 rescaling method

--- src/preprocessing/filtering.py
+++ src/preprocessing/filtering.py
@@ -3,35 +3,43 @@ import numpy as np
 from scipy import signal
 
 
-def filter_signal(sfreq: int, data: np.array) -> np.array:
+def filter_signal(sfreq: int, cutoff_freq_low: int, cutoff_freq_high: int, data: np.array, zero_center: bool = True) \
+        -> np.array:
     """
-    Filter the provided signal with a low-pass butterworth forward-backward filter
-    at cut-off frequency 200 Hz. Additionally zero-centers the data
+    Filter the provided signal with a bandpass butterworth forward-backward filter
+    at specified cut-off frequencies. The order of the butterworth filter is predefined to be 2,
+    which effectively results in an order of 4 as the data is forward-backward filtered.
+    Additionally, the possibility to zero-center the data is provided.
 
     :param sfreq: sampling frequency of the input signal/-s
+    :param cutoff_freq_low: lower end of the frequency passband
+    :param cutoff_freq_high: upper end of the frequency passband
     :param data: signal/-s to be filtered
-    :return: low-pass filtered zero-centered signal/-s at cut-off frequency 200 Hz
+    :param zero_center: if True, re-centers the signal/-s, defaults to True
+    :return: bandpass filtered zero-centered signal/-s at cut-off frequency 200 Hz
     """
     # Nyquist frequency (i.e. half the sampling frequency)
     nyq = sfreq / 2
 
     # cut-off frequencies
-    cutoff_freq_low = 1
-    cutoff_freq_high = 200
+    f_l = cutoff_freq_low
+    f_h = cutoff_freq_high
 
     # Normalize frequency
-    norm_freq = np.array(cutoff_freq_low, cutoff_freq_high) / nyq
+    norm_freq = np.array(f_l, f_h) / nyq
 
     # create an iir (infinite impulse response) butterworth filter
     iir_params = dict(order=2, ftype="butter")
     iir_filtered = mne.filter.create_filter(
-        data, nyq, l_freq=norm_freq[0], h_freq=norm_freq[1], method="iir", iir_params=iir_params, verbose=True
+        data, nyq, l_freq=norm_freq[0], h_freq=norm_freq[1], method="iir", iir_params=iir_params, btype="bandpass",
+        verbose=True
     )
 
     # forward-backward filter
-    data_steep = signal.sosfiltfilt(iir_filtered["sos"], data)
+    filtered_eeg = signal.sosfiltfilt(iir_filtered["sos"], data)
 
-    # zero-center the data
-    zero_centered = data_steep - np.median(data_steep, 1, keepdims=True)
+    if zero_center:
+        # zero-center the data
+        filtered_eeg -= np.median(filtered_eeg, 1, keepdims=True)
 
-    return zero_centered
+    return filtered_eeg
