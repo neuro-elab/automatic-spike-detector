9c7f7cfce919501d436303d5a280de0e88434f60
Dominik Frey <frey@brickwork.ch>
2023-08-25 09:56:47 +0200

    asd-2 basic filter

--- src/preprocessing/filter.py
+++
@@ -1,6 +0,0 @@
-import mne
-
-
-# create bandpass filter for 1 - 200 Hz
-def filter_signal(data):
-    pass
---
+++ src/preprocessing/filtering.py
@@ -0,0 +1,27 @@
+import mne
+import numpy as np
+from scipy import signal
+
+
+# create bandpass filter for 1 - 200 Hz
+def filter_signal(sfreq: int, data: np.array) -> np.array:
+    # Nyquist frequency
+    nyq = sfreq / 2
+
+    # cut-off frequencies
+    cutoff_freq_low = 1
+    cutoff_freq_high = 200
+
+    # Normalize frequency
+    norm_freq = np.array(cutoff_freq_low, cutoff_freq_high) / nyq
+
+    # create an iir (infinite impulse response) butterworth filter
+    iir_params = dict(order=2, ftype="butter")
+    iir_filtered = mne.filter.create_filter(
+        data, nyq, l_freq=norm_freq[0], h_freq=norm_freq[1], method="iir", iir_params=iir_params, verbose=True
+    )
+
+    # forward-backward filter
+    data_steep = signal.sosfiltfilt(iir_filtered["sos"], data)
+
+    return data_steep
