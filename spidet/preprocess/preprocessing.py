be3309a1b506a67426d9b697b701b1b78dfbfdef
DominikFischli <dominikfischli@bluewin.ch>
2025-05-07 15:20:51 +0200

    accept ndarray instead of Trace objects

--- spidet/preprocess/preprocessing.py
+++ spidet/preprocess/preprocessing.py
@@ -1,16 +1,14 @@
-from typing import List
-
 import numpy as np
-from loguru import logger
 
-from spidet.domain.Trace import Trace
 from spidet.preprocess.filtering import filter_signal, notch_filter_signal
 from spidet.preprocess.resampling import resample_data
 from spidet.preprocess.rescaling import rescale_data
 
 
 def apply_preprocessing_steps(
-    traces: List[Trace],
+    channel_names: list,
+    sfreq: int,
+    data: np.ndarray,
     notch_freq: int,
     resampling_freq: int,
     bandpass_cutoff_low: int,
@@ -26,8 +24,11 @@ def apply_preprocessing_steps(
 
     Parameters
     ----------
-    traces : List[Trace]
-        The original iEEG data as a list of Traces objects. Each trace corresponds to the recording of single channel.
+    channel_names : list
+        The channel names as strings corresponding to the rows in data. The number of channel names should be equal to the number of rows in data.
+
+    data : np.ndarray
+        The data matrix representing eeg traces to be preprocessed. The data will be rowwise scaled and filtered.
 
     notch_freq : int
         The frequency of the notch filter; data will be notch-filtered at this frequency
@@ -49,44 +50,33 @@ def apply_preprocessing_steps(
         2-dimensional numpy array containing the preprocessed data where the rows correspond to the input traces.
     """
 
-    # Extract channel names
-    channel_names = [trace.label for trace in traces]
-
-    logger.debug(f"Channels processed by worker: {channel_names}")
-
-    # Extract data sampling freq
-    sfreq = traces[0].sfreq
-
-    # Extract raw data from traces
-    traces = np.array([trace.data for trace in traces])
-
     # 1. Bandpass filter
-    bandpass_filtered = filter_signal(
+    processed = filter_signal(
         sfreq=sfreq,
         cutoff_freq_low=bandpass_cutoff_low,
         cutoff_freq_high=bandpass_cutoff_high,
-        data=traces,
+        data=data,
     )
 
     # 2. Notch filter
-    notch_filtered = notch_filter_signal(
-        eeg_data=bandpass_filtered,
+    processed = notch_filter_signal(
+        eeg_data=processed,
         notch_frequency=notch_freq,
         low_pass_freq=bandpass_cutoff_high,
         sfreq=sfreq,
     )
 
     # 3. Scaling channels
-    scaled_data = rescale_data(
-        data_to_be_scaled=notch_filtered, original_data=traces, sfreq=sfreq
+    processed = rescale_data(
+        data_to_be_scaled=processed, original_data=data, sfreq=sfreq
     )
 
     # 4. Resampling data
-    resampled_data = resample_data(
-        data=scaled_data,
+    processed = resample_data(
+        data=processed,
         channel_names=channel_names,
         sfreq=sfreq,
         resampling_freq=resampling_freq,
     )
 
-    return resampled_data
+    return processed
